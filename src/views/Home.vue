08338419744ac3d7db0be7f82b8fae6367b5decd
Keelana Char <keelanachar@gmail.com>
2019-12-10 21:12:01 -0600

    new/create action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+
+    <p>Product name <input type="text" v-model="productName"></p>
+    <p>Product description <input type="text" v-model="productDescription"></p>
+    <p>Product price <input type="text" v-model="productPrice"></p>
+
+    <button v-on:click="createProduct()">Create Product</button>
+
     <div v-for="product in products">
       <p> {{ product.name }}</p>
       <p> {{ product.description }}</p>
@@ -21,7 +28,10 @@ export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      products: []
+      products: [],
+      productName: "",
+      productDescription: "",
+      productPrice: ""
     };
   },
   created: function() {
@@ -31,6 +41,24 @@ export default {
       this.products = response.data;
     });
   },
-  methods: {}
+  methods: {
+    createProduct: function() {
+      console.log('creating product');
+
+      var params = {
+        name: this.productName,
+        description: this.productDescription,
+        price: this.productPrice
+      };
+
+      axios.post('/api/products', params).then(response => {
+        console.log(response.data);
+        this.products.push(response.data);
+        this.productName = "";
+        this.productDescription = "";
+        this.productPrice = "";
+      });
+    }
+  }
 };
 </script>
