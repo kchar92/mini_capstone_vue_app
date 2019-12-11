d03357ac4f00410d72a8d3167735380ac33e8bee
Keelana Char <keelanachar@gmail.com>
2019-12-10 19:39:23 -0600

    use axios to get data from rails app.. index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="product in products">
+      <p> {{ product.name }}</p>
+      <p> {{ product.description }}</p>
+      <hr>
+    </div>
+    
   </div>
 </template>
 
@@ -8,13 +14,21 @@
 </style>
 
 <script>
+import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      products: []
     };
   },
-  created: function() {},
+  created: function() {
+    console.log('created here');
+    axios.get('/api/products').then(response => {
+      console.log(response.data);
+      this.products = response.data;
+    });
+  },
   methods: {}
 };
 </script>
